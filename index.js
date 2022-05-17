50cf04234031f6aaf3bcd6338d839e78a6bd80da
朱修相 <zxx960@qq.com>
2022-05-17 16:47:22 +0800

    feat:支持settings.json中配置路径

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const fs = require('fs')
 
 const FILE = vscode.CompletionItemKind.File
 const FOLDER = vscode.CompletionItemKind.Folder
-const imaType = ['.png', '.jpg', '.jpeg', '.gif', '.bmp', '.webp', '.svg', '.ico']// 图片格式
+const imaType = ['.png', '.jpg', '.jpeg', '.gif', '.bmp', '.webp', '.svg', '.ico']  //支持的图片格式
 /**
  * [isdir 判断目标是否为目录]
  */
@@ -16,20 +16,12 @@ function isdir(path) {
   }
 }
 
-function isfile(path) {
-  try {
-    return fs.statSync(path).isFile()
-  } catch (err) {
-    return false
-  }
-}
-
 /**
  * 列出目录
  */
 function ls(dir) {
   try {
-    var list = fs.readdirSync(dir)
+    let list = fs.readdirSync(dir)
     return list.filter(it => !it.startsWith('.')).map(it => resolve(dir, it))
   } catch (err) {
     return []
@@ -37,9 +29,9 @@ function ls(dir) {
 }
 
 function getPrefixTxt(line, idx) {
-  var txt = line.slice(0, idx)
-  var n = txt.lastIndexOf('"') > -1 ? txt.lastIndexOf('"') : txt.lastIndexOf("'")
-  var r
+  let txt = line.slice(0, idx)
+  let n = txt.lastIndexOf('"') > -1 ? txt.lastIndexOf('"') : txt.lastIndexOf("'")
+  let r
 
   txt = txt.slice(n + 1)
   if (txt) {
@@ -55,22 +47,20 @@ function getPrefixTxt(line, idx) {
 
 function famatePath(path) {
   const isExtendedLengthPath = /^\\\\\?\\/.test(path);
-  const hasNonAscii = /[^\u0000-\u0080]+/.test(path); // eslint-disable-line no-control-regex
-
+  const hasNonAscii = /[^\u0000-\u0080]+/.test(path);
   if (isExtendedLengthPath || hasNonAscii) {
     return path;
   }
-
   return path.replace(/\\/g, '/');
 };
 
 function item(text, type, p, path) {
-  var CompletionItem = new vscode.CompletionItem(text, type)
+  let CompletionItem = new vscode.CompletionItem(text, type)
   CompletionItem.range = new vscode.Range(p, p)
   if (path) {
     imaType.forEach(item => {
       if (path.endsWith(item)) {
-        let image = `![image](${vscode.Uri.file(famatePath(path))}|width=200)`
+        let image = `&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;![image](${vscode.Uri.file(famatePath(path))}|width=200)`
         CompletionItem.documentation = new vscode.MarkdownString(image)
       }
     })
@@ -78,21 +68,24 @@ function item(text, type, p, path) {
   return CompletionItem
 }
 
+let rootPath = vscode.workspace.workspaceFolders[0].uri.fsPath
+let mappings = vscode.workspace.getConfiguration('path-prompt').mappings
 let options = {
-  workspace: resolve('./'),
-  extendWorkspace: null // 额外的项目目录, 一般是 vue项目中的 src目录
+  workspace: rootPath,
+  extendWorkspace: {
+    '@/': join(rootPath, 'src/'),
+  }
 }
-
+for (let key in mappings) {
+  options.extendWorkspace[key] = join(rootPath, mappings[key].replace('${workspaceRoot}', ''))
+}
 class AutoPath {
   provideCompletionItems(doc, pos) {
-    var currDir = dirname(doc.fileName)
-    var inputTxt = doc.lineAt(pos).text // 获取光标所在的整行代码
-    var list = []
-    var currDirFixed = ''
-    var inputTxtTrim = inputTxt.trim()
-
-    // console.log('原始inputTxt >>>> ', inputTxt)
-
+    let currDir = dirname(doc.fileName)
+    let inputTxt = doc.lineAt(pos).text // 获取光标所在的整行代码
+    let list = []
+    let currDirFixed = ''
+    let inputTxtTrim = inputTxt.trim()
     /**
      * 过滤掉 以下几种情况
      * @1 在注释后面的
@@ -113,9 +106,6 @@ class AutoPath {
 
     inputTxt = getPrefixTxt(inputTxt, pos.character)
     currDirFixed = join(currDir, inputTxt)
-
-    // console.log('修正后的inputTxt: ', inputTxt, currDirFixed)
-
     if (!inputTxt) {
       return
     }
@@ -123,23 +113,13 @@ class AutoPath {
     if (inputTxt.startsWith('./') || inputTxt.startsWith('../')) {
       list.push(...ls(currDirFixed))
     } else {
-
-      // vue项目
-      if (inputTxt.startsWith('@/') && options.extendWorkspace) {
-        currDirFixed = join(options.extendWorkspace, inputTxt.slice(2))
-        list.push(...ls(currDirFixed))
-      } else if (inputTxt.startsWith('~')) {
-        currDirFixed = inputTxt.replace(/^~/, process.env.HOME)
-        list.push(...ls(currDirFixed))
-      }
-      // 其他的
-      else {
-        currDirFixed = join(options.workspace, inputTxt)
-        list.push(...ls(currDirFixed))
+      for (let key in options.extendWorkspace) {
+        if (inputTxt.startsWith(key)) {
+          currDirFixed = join(options.extendWorkspace[key], inputTxt.slice(key.length))
+          list.push(...ls(currDirFixed))
+        }
       }
     }
-
-    // console.log('currDirFixed: ', options, currDirFixed, list)
 
     list = list
       .filter(it => it !== doc.fileName)
@@ -157,34 +137,11 @@ class AutoPath {
   }
 }
 
-function __init__() {
-  let folders = vscode.workspace.workspaceFolders
-
-  if (folders && folders.length) {
-    options.workspace = folders[0].uri.fsPath
-  }
-
-  if (options.workspace) {
-
-    // 简单判断是否是vue项目
-    if (isfile(join(options.workspace, 'package.json'))) {
-      let conf = require(join(options.workspace, 'package.json'))
-      if (conf.dependencies && conf.dependencies.vue) {
-        let extendWorkspace = join(options.workspace, 'src/')
-        if (isdir(extendWorkspace)) {
-          options.extendWorkspace = extendWorkspace
-        }
-      }
-    }
-  }
-}
-
 exports.activate = function (ctx) {
-  // console.log('启动成功')
-  __init__()
   let ap = new AutoPath()
   let auto = vscode.languages.registerCompletionItemProvider('*', ap, '"', "'", '/')
   ctx.subscriptions.push(auto)
+  console.log('启动成功')
 }
 
 exports.deactivate = function () { }
